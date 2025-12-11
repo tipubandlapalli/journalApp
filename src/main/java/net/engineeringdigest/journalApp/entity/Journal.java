6948677ce1e4ce40c57a253f5e3c518a299fb373
tipubandlapalli <bstippusultan@gmail.com>
2025-12-11 20:02:09 +0530

    added lombok to reduce boiler code of entity

--- src/main/java/net/engineeringdigest/journalApp/entity/Journal.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/Journal.java
@@ -1,41 +1,17 @@
 package net.engineeringdigest.journalApp.entity;
 
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Document(collection = "journal_entries")
+@Data
 public class Journal {
     @Id
     private ObjectId id;
     private String content;
     private String title;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
 }
