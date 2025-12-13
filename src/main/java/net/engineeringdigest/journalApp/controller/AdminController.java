c644274eebbea19022a37e3a33844571d9c71ccb
tipubandlapalli <bstippusultan@gmail.com>
2025-12-13 15:10:36 +0530

    use of profile to restrict code on profiles - learned enum well

--- src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
@@ -1,8 +1,10 @@
 package net.engineeringdigest.journalApp.controller;
 
 import net.engineeringdigest.journalApp.entity.UserEntity;
+import net.engineeringdigest.journalApp.enums.Profiles;
 import net.engineeringdigest.journalApp.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("admin")
+@Profile(Profiles.DEV_PROFILE) // <-- FIX HERE
 public class AdminController {
     @Autowired
             private UserService userService;
-
     @GetMapping("all-users")
     public ResponseEntity<List<UserEntity>> getAllUsers(){
         return userService.getAllUsers();
---
+++ src/main/java/net/engineeringdigest/journalApp/enums/Profiles.java
@@ -0,0 +1,21 @@
+package net.engineeringdigest.journalApp.enums;
+
+
+public enum  Profiles {
+    NEVER_USE_ME("NEVER_USE_ME_rely_only_on_finals");
+
+    public static final String DEV_PROFILE = "dev";
+    public static final String PROD_PROFILE = "prod";
+
+    private final String value;
+
+    Profiles(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    // You can remove the unused getValues() method if it was for String[]
+}
